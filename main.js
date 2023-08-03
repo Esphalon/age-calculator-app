4a3b2041284ab7e5b4d219e456958cc378efe198
Esphalon <74604725+Esphalon@users.noreply.github.com>
2023-08-03 19:49:52 +0200

    Set up functionality age calculator

--- main.js
+++ main.js
@@ -3,23 +3,50 @@ const calcYear = document.querySelector(".calc-year");
 const calcMonth = document.querySelector(".calc-month");
 const calcDay = document.querySelector(".calc-day");
 
-const dateTime = new Date();
-const currentYear = dateTime.getFullYear();
-const currentMonth = dateTime.getMonth() + 1;
-const currentDay = dateTime.getDate();
-console.log(currentYear);
-console.log(currentMonth);
-console.log(currentDay);
+let setDay = 0;
+const setMonth = 0;
+const setYear = 0;
 
-const calculateAge = () => {
+const dateYear = new Date().getFullYear();
+const dateMonth = new Date().getMonth() +1;
+const dateDay = new Date().getDate();
+console.log(`${dateYear}/${dateMonth}/${dateDay}`);
+
+const grabInput = () => {
   // const inputYear = document.getElementById("input-year").value;
   const inputYear = document.getElementById("input-year").value;
-  const inputDay = document.getElementById("input-day").value;
   const inputMonth = document.getElementById("input-month").value;
-  calcYear.innerHTML = currentYear - inputYear;
-  calcMonth.innerHTML = currentMonth - inputMonth;
-  calcDay.innerHTML = currentDay - inputDay;
+  const inputDay = document.getElementById("input-day").value;
+
+  console.log(`${inputYear}/${inputMonth}/${inputDay}`);
+
+  const currentDate = new Date(dateYear, dateMonth -1, dateDay); 
+  const givenDate = new Date(inputYear, inputMonth -1, inputDay);
+  
+   // Calculate the difference in years
+   let yearsDifference = dateYear - inputYear;
+
+   // Calculate the difference in months and adjust years accordingly
+  let monthsDifference = dateMonth - inputMonth;
+  if (dateDay < inputDay) {
+    monthsDifference--;
+  }
+  // Calculate the difference in days
+  let daysDifference = dateDay - inputDay;
+  if (daysDifference < 0) {
+    // If the current day is less than the given day, adjust months and days
+    monthsDifference--;
+    const lastMonth = new Date(dateYear, dateMonth, 0).getDate();
+    daysDifference = lastMonth + daysDifference;
+  }
+   // If the current month is less than the given month, adjust years
+   if (monthsDifference < 0) {
+    yearsDifference--;
+    monthsDifference += 12;
+  }
+  calcYear.innerHTML = yearsDifference;
+  calcMonth.innerHTML = monthsDifference;
+  calcDay.innerHTML = daysDifference;
 }
 
-
-sendDate.addEventListener('click', calculateAge)
+sendDate.addEventListener('click', grabInput)
